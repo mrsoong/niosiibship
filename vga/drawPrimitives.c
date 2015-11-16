614a1275a76a05b9ce53e76a69c385bf61aa3365
steven <steven.song@mail.utoronto.ca>
2015-11-15 21:29:55 -0500

    Renamed drawpixel to fillpixel and began writing a drawPrimitives function

---
+++ vga/drawPrimitives.c
@@ -0,0 +1,15 @@
+/* A Bresenham's line algorithm based off the pseudocode 
+ * on https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
+ * is used to implement the drawLine function
+ */
+#include <stdio.h>
+
+/*
+ * Draws a line w/ the fillpixel subroutine
+ * Note that the x coordinate is the column (i.e 0<=x<320)
+ * and the y coordinate is the row (i.e 0<=y<240)
+ */
+void drawLine (int x0, int y0, int x1, int y1, long rgb) {
+	
+}
+
