043dcd394d631f7f5812b2f11cb8abd915b49922
ychenwang <wang.yucan@gmail.com>
2015-11-25 13:56:09 -0500

    Fixed potential infinite loop
    Possible syntax errors

--- AI.c
+++ AI.c
@@ -45,11 +45,30 @@ int getidx() {
 	return rand() % 8;
 }
 
+int *chose_rand_square() {
+	int valid = 0;
+	int x;
+	int y;
+	static int result[2];
+
+	while(valid != 0) {
+			x = getidx();
+			y = getidx();
+			valid = check_coord(x, y);
+		}
+
+	result[GRID_X] = x;
+	result[GRID_Y] = y;
+
+	return result;
+}
+
 int *chose_square() {
 	int x;
 	int y;
 	int valid = 0;
 	static int result[2];
+	int temp_result[2];
 
 	if (hit_prev) {
 		while (valid != 0) {
@@ -59,42 +78,46 @@ int *chose_square() {
 			switch(rand() % 4) {
 				//Above the previous square.
 				case 0:
-					if (previous[GRID_Y] != MIN_IDX) {
-						x = previous[GRID_X];
-						y = previous[GRID_Y] - 1;
+					x = previous[GRID_X];
+					y = previous[GRID_Y] - 1;
+					if (previous[GRID_Y] != MIN_IDX) & check_coord(x, y)) {
 						break;
 					}
 				//Left of the previous square.
 				case 1:
-					if (previous[GRID_X] != MIN_IDX) {
-						x = previous[GRID_X] - 1;
-						y = previous[GRID_Y];
+					x = previous[GRID_X] - 1;
+					y = previous[GRID_Y];
+					if (previous[GRID_X] != MIN_IDX & check_coord(x, y)) {
 						break;
 					}
 				//Below the previous square.
 				case 2:
-					if (previous[GRID_Y] != MAX_IDX) {
-						x = previous[GRID_X];
-						y = previous[GRID_Y] + 1;
+					x = previous[GRID_X];
+					y = previous[GRID_Y] + 1;
+					if (previous[GRID_Y] != MAX_IDX & check_coord(x, y)) {
+						
 						break;
 					}
 				//Right of the previous square.
 				case 3:
-					if (previous[GRID_X] != MAX_IDX) {
-						x = previous[GRID_X] + 1;
-						y = previous[GRID_Y];
+					x = previous[GRID_X] + 1;
+					y = previous[GRID_Y];
+					if (previous[GRID_X] != MAX_IDX & check_coord(x, y)) {
 						break;
 					}
+				//If it's never valid.
+				default:
+					temp_result = chose_rand_square();
+					x = temp_result[GRID_X];
+					y = temp_result[GRID_Y];
 			}
 			valid = check_coord(x, y);
 		}
 	}
 	else {
-		while(valid != 0) {
-			x = getidx();
-			y = getidx();
-			valid = check_coord(x, y);
-		}
+		temp_result = chose_rand_square();
+		x = temp_result[GRID_X];
+		y = temp_result[GRID_Y];
 	}
 
 	hit_prev = check_hit(x, y);
